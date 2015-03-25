4d0747b4a69ea0c47d57e0468612a28bcc596309
mauro-ferrario <hello@mauroferrario.com>
2015-03-25 10:28:33 +0100

    Refactoring

--- src/OverlayTextElement.h
+++ src/OverlayTextElement.h
@@ -14,9 +14,9 @@
 class OverlayTextElement: public OverlayElement
 {
 public:
-        OverlayTextElement();
-        OverlayTextElement(float introDuration, float introDelay, float outroDuration, float outroDelay, string text, ofTrueTypeFont* font, ofVec2f position);
-  void  initTexts(ofTrueTypeFont* font);
+                  OverlayTextElement();
+                  OverlayTextElement(float introDuration, float introDelay, float outroDuration, float outroDelay, string text, ofTrueTypeFont* font, ofVec2f position);
+  void            initTexts(ofTrueTypeFont* font);
   ofTrueTypeFont*	font;
   string          text;
   void            draw();
